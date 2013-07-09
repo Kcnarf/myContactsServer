383610e9cb25cabeb102f1389b23d1ff103f8772
Kcnarf <fl.franck.lebeau@gmail.com>
2013-07-09 17:56:20 +0200

    compute response to GET '/contacts'

--- controllers/usersController.php
+++ controllers/usersController.php
@@ -7,20 +7,46 @@ class UsersController extends RootController
             $user_id = (int)$request->url_elements[2];
             if(isset($request->url_elements[3])) {
                 switch($request->url_elements[3]) {
-                case 'friends':
-                    $data["message"] = "user " . $user_id . "has many friends";
+                case 'groups':
+                    $data["message"] = "user " . $user_id . " is in many groups";
                     break;
                 default:
                     // do nothing, this is not a supported action
                     break;
                 }
             } else {
-                $data["message"] = "here is the info for user " . $user_id;
+              $data["message"] = "here is the info for user " . $user_id;
             }
         } else {
-            $data["message"] = "you want a list of users";
+					//$data["message"] = "you want a list of users";
+					
+					$mysqli = $this->opendb();
+					if (mysqli_connect_errno()) {
+						printf("Connect failed: %s\n", mysqli_connect_error());
+						exit();
+					}
+
+					// A QUICK QUERY ON A FAKE USER TABLE
+					$query = "SELECT contact_id FROM `contacts`";
+					$mysqliresult = $mysqli->query($query) or die($mysqli->error.__LINE__);
+
+					// GOING THROUGH THE DATA
+					if($mysqliresult->num_rows > 0) {
+						$jsonedRows = array();
+						while($row = $mysqliresult->fetch_assoc()) {
+							$jsonedRow["id"] = $row["contact_id"];
+							$jsonedRows[] = $jsonedRow;
+						}
+						$jsonResult["contacts"] = $jsonedRows;
+					}
+					else {
+						$jsonResult["message"] = "NO RESULTS";	
+					}
+						
+					// CLOSE CONNECTION
+					mysqli_close($mysqli);
         }
-        return $data;
+        return $jsonResult;
     }
 
     public function postAction($request) {
