a1bc8c0ee270ee57b858ad3cbbe47dec3826faa9
Kcnarf <fl.franck.lebeau@gmail.com>
2013-07-10 17:17:22 +0200

    add responses to all verbs, manage update/creation of Group

--- controllers/groupsController.php
+++ controllers/groupsController.php
@@ -12,7 +12,7 @@ class GroupsController extends RootController
 					exit();
 				}
 				
-				$query = "SELECT * FROM `groups` WHERE group_id =" . $object_id;
+				$query = "SELECT * FROM `mycontacts`.`groups` WHERE `group_id` =" . $object_id;
 				$mysqliResult = $mysqli->query($query) or die($mysqli->error.__LINE__);
 
 				// GOING THROUGH THE DATA
@@ -22,7 +22,7 @@ class GroupsController extends RootController
 						$arrayedRow["name"] = $row["name"];
 					}
 				}
-				$emberStructuredResult["group"] = $arrayedRow;				
+				$emberStructuredResult["group"] = $arrayedRow;
 				
 				// CLOSE CONNECTION
 				mysqli_free_result($mysqliResult);
@@ -37,7 +37,7 @@ class GroupsController extends RootController
 					exit();
 				}
 				
-				$query = "SELECT * FROM `groups`";
+				$query = "SELECT * FROM `mycontacts`.`groups`";
 				$mysqliResult = $mysqli->query($query) or die($mysqli->error.__LINE__);
 
 				// GOING THROUGH THE DATA
@@ -62,8 +62,33 @@ class GroupsController extends RootController
 
 	public function postAction($request) {
 		if(isset($request->parameters)) {
-			$emberStructuredResult = $request->parameters;
-			// $emberStructuredResult["message"] = "Request to create an object";
+			$objectProperties = $request->parameters;
+			$mysqli = $this->opendb();
+			if (mysqli_connect_errno()) {
+				printf("Connect failed: %s\n", mysqli_connect_error());
+				exit();
+			}
+			
+			$query =  "INSERT INTO `mycontacts`.`groups` (`name`) VALUES ('" . $objectProperties["group"]->name . "')";
+			$mysqli->query($query) or die($mysqli->error.__LINE__);
+			
+			$object_id = $mysqli->insert_id;
+			$query = "SELECT * FROM `mycontacts`.`groups` WHERE `group_id` = " . $object_id;
+			$mysqliResult = $mysqli->query($query) or die($mysqli->error.__LINE__);
+
+			// GOING THROUGH THE DATA
+			if($mysqliResult->num_rows > 0) {
+				while($row = $mysqliResult->fetch_assoc()) {
+					$arrayedRow["id"] = $row["group_id"];
+					$arrayedRow["name"] = $row["name"];
+				}
+			}
+			$emberStructuredResult["group"] = $arrayedRow;
+			
+			// CLOSE CONNECTION
+			
+			mysqli_close($mysqli);
+			mysqli_free_result($mysqliResult);
 			return $emberStructuredResult;
 		}
 		else {
@@ -74,9 +99,34 @@ class GroupsController extends RootController
 	
 	public function putAction($request) {
 		if(isset($request->url_elements[2])) {
-			$object_id = (int)$request->url_elements[2];
-			$emberStructuredResult = $request->parameters;
-			// $emberStructuredResult["message"] = "Request to update object with id: " . $object_id;
+			$object_id=$request->url_elements[2];
+			$objectProperties = $request->parameters;
+			$mysqli = $this->opendb();
+			if (mysqli_connect_errno()) {
+				printf("Connect failed: %s\n", mysqli_connect_error());
+				exit();
+			}
+			
+			$query =  "UPDATE `mycontacts`.`groups` SET `name` = '" . $objectProperties["group"]->name . "' WHERE `groups`.`group_id` = " . $object_id . ";";
+			$mysqli->query($query) or die($mysqli->error.__LINE__);
+			
+			$query = "SELECT * FROM `groups` WHERE `group_id` =" . $object_id;
+			$mysqliResult = $mysqli->query($query) or die($mysqli->error.__LINE__);
+
+			// GOING THROUGH THE DATA
+			if($mysqliResult->num_rows > 0) {
+				while($row = $mysqliResult->fetch_assoc()) {
+					$arrayedRow["id"] = $row["group_id"];
+					$arrayedRow["name"] = $row["name"];
+				}
+			}
+			$emberStructuredResult["group"] = $arrayedRow;
+			
+			// CLOSE CONNECTION
+			
+			mysqli_close($mysqli);
+			mysqli_free_result($mysqliResult);
+			
 			return $emberStructuredResult;
 		} else {
 			// do nothing, this is not a supported action
@@ -94,13 +144,13 @@ class GroupsController extends RootController
 				exit();
 			}
 			
-			$query = "Delete FROM `groups` WHERE group_id = " . $object_id;
+			$query = "Delete FROM `mycontacts`.`groups` WHERE `group_id` = " . $object_id;
 			$mysqliResult = $mysqli->query($query) or die($mysqli->error.__LINE__);
 			
 			// CLOSE CONNECTION
 			mysqli_close($mysqli);
 			
-			// $emberStructuredResult["message"] = "Request to delete object with id: " . $object_id;
+			//$emberStructuredResult["message"] = "Request to delete object with id: " . $object_id;
 			return $emberStructuredResult;
 		} else {
 			// do nothing, this is not a supported action
--- libs/Request.php
+++ libs/Request.php
@@ -28,32 +28,33 @@ class Request {
 
         // now how about PUT/POST bodies? These override what we got from GET
         $body = file_get_contents("php://input");
+				
         $content_type = false;
         if(isset($_SERVER['CONTENT_TYPE'])) {
             $content_type = $_SERVER['CONTENT_TYPE'];
         }
-        switch($content_type) {
-            case "application/json":
-                $body_params = json_decode($body);
-                if($body_params) {
-                    foreach($body_params as $param_name => $param_value) {
-                        $parameters[$param_name] = $param_value;
-                    }
-                }
-                $this->format = "json";
-                break;
-            case "application/x-www-form-urlencoded":
-                parse_str($body, $postvars);
-                foreach($postvars as $field => $value) {
-                    $parameters[$field] = $value;
+				
+        if (stristr($content_type, "application/json")){
+					$body_params = json_decode($body);
+					$parameters["body_params"] = $body_params;
+					if($body_params) {
+						foreach($body_params as $param_name => $param_value) {
+							$parameters[$param_name] = $param_value;
+						}
+					}
+					$this->format = "json";
+				} else if (stristr($content_type, "application/x-www-form-urlencoded")){
+					parse_str($body, $postvars);
+					foreach($postvars as $field => $value) {
+							$parameters[$field] = $value;
 
-                }
-                $this->format = "html";
-                break;
-            default:
-                // we could parse other supported formats here
-                break;
-        }
+					}
+					$this->format = "html";
+				} else {
+					$parameters["body_params"] = "unsupported content type";
+					// we could parse other supported formats here
+				}
+				
         $this->parameters = $parameters;
     }
 }
