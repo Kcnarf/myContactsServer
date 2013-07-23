0397b5344a21b7380dcc588441836f28ae699dd5
Kcnarf <fl.franck.lebeau@gmail.com>
2013-07-23 17:42:56 +0200

    manage contact_group_link model
    . create table contact_group_links (rename the unused
    manytomany_contacts_groups table)
    . add contact_group_linksController (PHP)

---
+++ controllers/contact_group_linksController.php
@@ -0,0 +1,108 @@
+<?php
+
+class Contact_group_linksController extends RootController
+{
+	public function getAction($request) {
+		if(isset($request->url_elements[2])) {
+			// do nothing, this is not a supported action
+			break;
+
+		} else {
+			$mysqli = $this->opendb();
+			if (mysqli_connect_errno()) {
+				printf("Connect failed: %s\n", mysqli_connect_error());
+				exit();
+			}
+			
+			$query = "SELECT * FROM `mycontacts`.`contact_group_links`";
+			$mysqliResult = $mysqli->query($query) or die($mysqli->error.__LINE__);
+
+			// GOING THROUGH THE DATA
+			$arrayedRows = array();
+			if($mysqliResult->num_rows > 0) {
+				while($row = $mysqliResult->fetch_assoc()) {
+					$arrayedRow["id"] = $row["id"];
+					$arrayedRow["contact_id"] = $row["contact_id"];
+					$arrayedRow["group_id"] = $row["group_id"];
+					$arrayedRows[] = $arrayedRow;
+				}
+			}
+			$emberStructuredResult["contact_group_links"] = $arrayedRows;	
+			
+			// CLOSE CONNECTION
+			mysqli_free_result($mysqliResult);
+			mysqli_close($mysqli);
+			
+			// $emberStructuredResult["message"] = "Request for list of objects";
+			return $emberStructuredResult;
+		}
+	}
+
+	public function postAction($request) {
+		if(isset($request->parameters)) {
+			$objectProperties = $request->parameters;
+			$mysqli = $this->opendb();
+			if (mysqli_connect_errno()) {
+				printf("Connect failed: %s\n", mysqli_connect_error());
+				exit();
+			}
+			
+			$query =  "INSERT INTO `mycontacts`.`groups` (`contact_id`,`group_id`) VALUES ('" . $objectProperties["contact_group_link"]->contact_id . "','" . $objectProperties["contact_group_link"]->contact_id . "')";
+			$mysqli->query($query) or die($mysqli->error.__LINE__);
+			
+			$object_id = $mysqli->insert_id;
+			$query = "SELECT * FROM `mycontacts`.`contact_group_links` WHERE `id` = " . $object_id;
+			$mysqliResult = $mysqli->query($query) or die($mysqli->error.__LINE__);
+
+			// GOING THROUGH THE DATA
+			if($mysqliResult->num_rows > 0) {
+				while($row = $mysqliResult->fetch_assoc()) {
+					$arrayedRow["id"] = $row["id"];
+					$arrayedRow["contact_id"] = $row["contact_id"];
+					$arrayedRow["group_id"] = $row["group_id"];
+				}
+			}
+			$emberStructuredResult["contact_group_link"] = $arrayedRow;
+			
+			// CLOSE CONNECTION
+			
+			mysqli_close($mysqli);
+			mysqli_free_result($mysqliResult);
+			return $emberStructuredResult;
+		}
+		else {
+			// do nothing, this is not a supported action
+			break;
+		}
+	}
+	
+	public function putAction($request) {
+		
+		// do nothing, this is not a supported action
+		break;
+	}
+	
+	public function deleteAction($request) {
+		if(isset($request->url_elements[2])) {
+			$object_id = (int)$request->url_elements[2];
+			
+			$mysqli = $this->opendb();
+			if (mysqli_connect_errno()) {
+				printf("Connect failed: %s\n", mysqli_connect_error());
+				exit();
+			}
+			
+			$query = "Delete FROM `mycontacts`.`contact_group_links` WHERE `id` = " . $object_id;
+			$mysqliResult = $mysqli->query($query) or die($mysqli->error.__LINE__);
+			
+			// CLOSE CONNECTION
+			mysqli_close($mysqli);
+			
+			//$emberStructuredResult["message"] = "Request to delete object with id: " . $object_id;
+			return $emberStructuredResult;
+		} else {
+			// do nothing, this is not a supported action
+			break;
+		}
+	}
+}
--- extras/myContactDatabaseStructure.sql
+++ extras/myContactDatabaseStructure.sql
@@ -2,10 +2,10 @@
 -- version 3.5.2.2
 -- http://www.phpmyadmin.net
 --
--- Client: 127.0.0.1
--- Généré le: Jeu 18 Juillet 2013 à 09:22
--- Version du serveur: 5.5.27
--- Version de PHP: 5.4.7
+-- Host: 127.0.0.1
+-- Generation Time: Jul 23, 2013 at 05:34 PM
+-- Server version: 5.5.27
+-- PHP Version: 5.4.7
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,57 +17,59 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Base de données: `mycontacts`
+-- Database: `mycontacts`
 --
 DROP DATABASE `mycontacts`;
-CREATE DATABASE `mycontacts` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+CREATE DATABASE `mycontacts` DEFAULT CHARACTER SET latin1 COLLATE latin1_general_cs;
 USE `mycontacts`;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `contacts`
+-- Table structure for table `contact_group_links`
+--
+
+DROP TABLE IF EXISTS `contact_group_links`;
+CREATE TABLE IF NOT EXISTS `contact_group_links` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `contact_id` int(11) NOT NULL,
+  `group_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contacts`
 --
 
 DROP TABLE IF EXISTS `contacts`;
 CREATE TABLE IF NOT EXISTS `contacts` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `alias` text CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
+  `alias` text COLLATE latin1_general_cs NOT NULL,
   `is_favorite` tinyint(1) NOT NULL,
-  `first_name` text CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
-  `last_name` text CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
-  `home_phone` text CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
-  `mobile_phone` text CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
-  `office_phone` text CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
-  `personal_mail` text CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
-  `office_mail` text CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
+  `first_name` text COLLATE latin1_general_cs NOT NULL,
+  `last_name` text COLLATE latin1_general_cs NOT NULL,
+  `home_phone` text COLLATE latin1_general_cs NOT NULL,
+  `mobile_phone` text COLLATE latin1_general_cs NOT NULL,
+  `office_phone` text COLLATE latin1_general_cs NOT NULL,
+  `personal_mail` text COLLATE latin1_general_cs NOT NULL,
+  `office_mail` text COLLATE latin1_general_cs NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs AUTO_INCREMENT=15 ;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `groups`
+-- Table structure for table `groups`
 --
 
 DROP TABLE IF EXISTS `groups`;
 CREATE TABLE IF NOT EXISTS `groups` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` text CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
+  `name` text COLLATE latin1_general_cs NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `manytomany_contacts_groups`
---
-
-DROP TABLE IF EXISTS `manytomany_contacts_groups`;
-CREATE TABLE IF NOT EXISTS `manytomany_contacts_groups` (
-  `contact_id` int(11) NOT NULL,
-  `group_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs AUTO_INCREMENT=6 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
